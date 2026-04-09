b1e41c154f9bdcecc39640933375469ae69405f5
amuda-egdk <amuda@eg.dk>
2026-04-09 13:50:03 +0530

    feat: add search-bar ui and add data input bar

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,9 +1,11 @@
+import AddData from "./components/AddData"
 import { ToDoSearch } from "./components/TodoSearch"
 
 export function App() {
   return (
     <div>
       <ToDoSearch />
+      <AddData />
     </div>
   )
 }
