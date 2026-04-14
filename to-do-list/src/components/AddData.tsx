44bb57bafc799d291b70e5d3d81b6b2f122fc491
amuda-egdk <amuda@eg.dk>
2026-04-14 17:13:52 +0530

    feat: item list with no same data

--- to-do-list/src/components/AddData.tsx
+++ to-do-list/src/components/AddData.tsx
@@ -1,32 +1,58 @@
-import { useState } from 'react'
-import { Input } from './ui/input'
+import { useState } from 'react';
+import { Input } from './ui/input';
 import { Button } from './ui/button';
 import ItemList from './ItemList';
+import { toast } from 'sonner';
 
 function AddData() {
   const [todos, setTodos] = useState<string[]>([]);
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState('');
 
-  const handleChange = (e : any) => {setInput(e.target.value);}
+  const handleChange = (e: any) => {
+    setInput(e.target.value);
+  };
 
   const handleAdd = () => {
-    if(input.trim() === "") return;
+    if (input.trim() === '') return;
 
-    setTodos([...todos, input]);
+    const caps = input.trim().charAt(0).toUpperCase() + input.trim().slice(1);
 
-    setInput("")
+    if (todos.includes(input.trim())) {
+      toast.error('This task already exists');
+      setInput('');
+      return;
+    }
+
+    setTodos([...todos, caps]);
+
+    setInput('');
+  };
+
+  const handleDelete = (indexToDelete: number) => {
+    setTodos(todos.filter((_, index) => index !== indexToDelete));
   };
 
   return (
-    <div className='flex items-center gap-2'>
-      <Input placeholder="Add new todo..." value={input} onChange={handleChange} onKeyDown={(e) => { if(e.key == "Enter"){ handleAdd();}}}/>
-      <Button onClick={handleAdd}>Add</Button>
+    <div>
+      <div className="flex items-center gap-2 p-2">
+        <Input
+          placeholder="Add new todo..."
+          value={input}
+          onChange={handleChange}
+          onKeyDown={e => {
+            if (e.key == 'Enter') {
+              handleAdd();
+            }
+          }}
+        />
+        <Button onClick={handleAdd}>Add</Button>
+      </div>
 
       <div>
-        <ItemList todos={todos}/>
+        <ItemList todos={todos} onDelete={handleDelete} />
       </div>
     </div>
-  )
+  );
 }
 
-export default AddData
+export default AddData;
--- to-do-list/src/components/ItemList.tsx
+++ to-do-list/src/components/ItemList.tsx
@@ -1,18 +1,25 @@
+import { Button } from './ui/button';
 
 type props = {
-    todos : string[];
+  todos: string[];
+  onDelete: (index: number) => void;
 };
 
-function ItemList({todos} : props) {
+function ItemList({ todos, onDelete }: props) {
   return (
-    <div>
+    <div className="p-5">
       {todos.map((todo, index) => (
-        <div key={index}>
-            {todo}
+        <div
+          key={index}
+          className="flex items-center justify-between mb-2 border border-blue-300 p-2"
+        >
+          <span className="text-xl text-blue-400">{todo}</span>
+
+          <Button onClick={() => onDelete(index)}>Delete</Button>
         </div>
       ))}
     </div>
   );
 }
 
-export default ItemList
+export default ItemList;
