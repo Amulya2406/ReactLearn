3900e42933b678cfcfca13174e9db6c3fa655300
Amulya2406 <137594859+Amulya2406@users.noreply.github.com>
2026-04-20 14:18:16 +0530

    feat: search filter (#5)
    Co-authored-by: amuda-egdk <amuda@eg.dk>

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,12 +1,11 @@
 import { Toaster } from 'sonner';
 import AddData from './components/AddData';
-import { ToDoSearch } from './components/TodoSearch';
 
 export function App() {
+
   return (
     <div>
-      <ToDoSearch />
-      <AddData />
+      <AddData/>
       <Toaster position="top-right" richColors />
     </div>
   );
